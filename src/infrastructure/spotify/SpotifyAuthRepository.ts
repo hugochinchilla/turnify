2f140a79cea8903fe44d3b539862547bceeec5fe
Hugo Chinchilla <hugoasecas@gmail.com>
2023-04-14 22:18:36 +0200

    refactor auth code

---
+++ src/infrastructure/spotify/SpotifyAuthRepository.ts
@@ -0,0 +1,104 @@
+import CryptoJS from "crypto-js";
+
+function generateRandomString(length: number) {
+  let text = "";
+  let possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
+
+async function generateCodeChallenge(codeVerifier: string) {
+  return CryptoJS.SHA256(codeVerifier)
+    .toString(CryptoJS.enc.Base64)
+    .replaceAll("+", "-")
+    .replaceAll("/", "_")
+    .replace(/=+$/, "");
+}
+
+function fetchAsFormEncoded(url: string, data: Record<string, string>) {
+  let body = new URLSearchParams(data);
+
+  return fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: body,
+  }).then((response) => {
+    if (!response.ok) {
+      throw new ErrorResponse("HTTP status " + response.status, response);
+    }
+    return response.json();
+  });
+}
+
+class ErrorResponse extends Error {
+  readonly response: Response;
+
+  constructor(message: string, response: Response) {
+    super(message);
+    this.response = response;
+  }
+}
+
+export class SpotifyAuthRepository {
+  constructor(private clientId: string, private redirectUri: string) {}
+
+  createCodeVerifier(): string {
+    return generateRandomString(128);
+  }
+
+  requestAccessToken(codeVerifier: string, code: string) {
+    return fetchAsFormEncoded("https://accounts.spotify.com/api/token", {
+      grant_type: "authorization_code",
+      code: code,
+      redirect_uri: this.redirectUri,
+      client_id: this.clientId,
+      code_verifier: codeVerifier,
+    });
+  }
+
+  refreshAccessToken(refreshToken: string) {
+    return fetchAsFormEncoded("https://accounts.spotify.com/api/token", {
+      grant_type: "refresh_token",
+      client_id: this.clientId,
+      refresh_token: refreshToken,
+    });
+  }
+
+  getProfile(accessToken: string) {
+    return fetch("https://api.spotify.com/v1/me", {
+      headers: {
+        Authorization: "Bearer " + accessToken,
+      },
+    }).then((response) => {
+      if (!response.ok) {
+        throw new ErrorResponse("HTTP status " + response.status, response);
+      }
+      return response.json();
+    });
+  }
+
+  redirectToLoginPage(codeVerifier: string) {
+    generateCodeChallenge(codeVerifier).then((codeChallenge) => {
+      let state = generateRandomString(16);
+      let scope = "user-read-private user-read-email streaming";
+
+      let args = new URLSearchParams({
+        response_type: "code",
+        client_id: this.clientId,
+        scope: scope,
+        redirect_uri: this.redirectUri,
+        state: state,
+        code_challenge_method: "S256",
+        code_challenge: codeChallenge,
+      });
+
+      window.location.assign("https://accounts.spotify.com/authorize?" + args);
+    });
+  }
+}
--- src/turnify/hooks/useAuthToken.ts
+++ src/turnify/hooks/useAuthToken.ts
@@ -1,103 +1,66 @@
-import CryptoJS from "crypto-js";
 import { useState } from "react";
+import { SpotifyAuthRepository } from "../../infrastructure/spotify/SpotifyAuthRepository";
 
 const clientId = "fd160f950e30436ebf69e14b9550cd0b";
 const redirectUri = "http://127.0.0.1:7878";
+const spotifyAuth = new SpotifyAuthRepository(clientId, redirectUri);
 
-function generateRandomString(length: number) {
-  let text = "";
-  let possible =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-  for (let i = 0; i < length; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
+function forceValidAppUrl() {
+  if (window.location.origin !== redirectUri) {
+    window.location.assign(redirectUri);
   }
-  return text;
 }
 
-async function generateCodeChallenge(codeVerifier: string) {
-  return CryptoJS.SHA256(codeVerifier)
-    .toString(CryptoJS.enc.Base64)
-    .replaceAll("+", "-")
-    .replaceAll("/", "_")
-    .replace(/=+$/, "");
+function getCodeVerifier(): string {
+  let codeVerifier = localStorage.getItem("code-verifier");
+  if (!codeVerifier) {
+    codeVerifier = spotifyAuth.createCodeVerifier();
+    localStorage.setItem("code-verifier", codeVerifier);
+  }
+
+  return codeVerifier;
 }
 
-function doLogin() {
-  const codeVerifier = generateRandomString(128);
-  localStorage.setItem("code-verifier", codeVerifier);
-
-  generateCodeChallenge(codeVerifier).then((codeChallenge) => {
-    let state = generateRandomString(16);
-    let scope = "user-read-private user-read-email streaming";
-
-    let args = new URLSearchParams({
-      response_type: "code",
-      client_id: clientId,
-      scope: scope,
-      redirect_uri: redirectUri,
-      state: state,
-      code_challenge_method: "S256",
-      code_challenge: codeChallenge,
-    });
-
-    window.location.assign("https://accounts.spotify.com/authorize?" + args);
-  });
-}
-
-function getAccessToken(): Promise<string> {
+function getAccessToken(code: string, codeVerifier: string): Promise<string> {
   const storedAccessToken = localStorage.getItem("access-token");
   if (storedAccessToken) {
     return Promise.resolve(storedAccessToken);
   }
 
-  const codeVerifier = localStorage.getItem("code-verifier") as string;
-  const urlParams = new URLSearchParams(window.location.search);
-  const code = urlParams.get("code") as string;
-
-  let body = new URLSearchParams({
-    grant_type: "authorization_code",
-    code: code,
-    redirect_uri: redirectUri,
-    client_id: clientId,
-    code_verifier: codeVerifier,
+  return spotifyAuth.requestAccessToken(codeVerifier, code).then((response) => {
+    localStorage.setItem("access-token", response.access_token);
+    localStorage.setItem("refresh-token", response.refresh_token);
+    return response.access_token;
   });
-
-  return fetch("https://accounts.spotify.com/api/token", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    body: body,
-  })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("HTTP status " + response.status);
-      }
-      return response.json();
-    })
-    .then((data) => {
-      localStorage.setItem("access-token", data.access_token);
-      return data.access_token;
-    })
 }
 
 export function useAuthToken() {
+  forceValidAppUrl();
+
   const [accessToken, setAccessToken] = useState<string | undefined>(undefined);
 
-  if (window.location.origin !== redirectUri) {
-    window.location.assign(redirectUri);
-  }
-
+  const codeVerifier = getCodeVerifier();
   const urlParams = new URLSearchParams(window.location.search);
   const code = urlParams.get("code");
 
   if (!code) {
-    doLogin();
+    spotifyAuth.redirectToLoginPage(codeVerifier);
   } else {
-    getAccessToken().then((token) => {
-      setAccessToken(token);
-    });
+    getAccessToken(code, codeVerifier)
+      .then((token) => {
+        spotifyAuth.getProfile(token).then((profile) => {
+          setAccessToken(token);
+        });
+      })
+      .catch((errorResponse) => {
+        errorResponse.response.json().then((response: any) => {
+          if (response.error === "invalid_grant") {
+            spotifyAuth.redirectToLoginPage(codeVerifier);
+          } else {
+            throw errorResponse;
+          }
+        });
+      });
   }
 
   return {
