c522a8f5f4951d58d2b3125a28269e277c01103e
Hugo Chinchilla <hugoasecas@gmail.com>
2023-04-19 23:19:57 +0200

    add some styles to tracklist

--- src/turnify/components/TrackList.tsx
+++ src/turnify/components/TrackList.tsx
@@ -1,15 +1,22 @@
 import { useAppContext } from "../contexts/AppContext";
+import { useSpotifyPlayer } from "react-spotify-web-playback-sdk";
 
 export const TrackList = () => {
   const { currentAlbum, trackList } = useAppContext();
+  const player = useSpotifyPlayer();
 
   return (
     currentAlbum && (
-      <div>
-        <img src={currentAlbum.artwork} alt={currentAlbum.title} />
-        <ul>
+      <div className={"flex justify-center align-middle grow p-4"}>
+        <img
+          src={currentAlbum.artwork}
+          alt={currentAlbum.title}
+          className={"m-4 object-contain h-48"}
+        />
+
+        <ul className={"text-slate-50 justify-center flex flex-col"}>
           {trackList.map((track) => (
-            <li key={track.trackNumber}>{track.title}</li>
+            <li key={track.title}>{track.title}</li>
           ))}
         </ul>
       </div>
